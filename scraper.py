744ebdb7eddf50ef2026782b5cd36b5ce7319b68
[Rick Zucker] <[rnz@uw.edu]>
2015-10-08 11:12:42 -0700

    Setting up repo; just entering basic docstring stuff

---
+++ scraper.py
@@ -0,0 +1,6 @@
+"""Practicing some basic web scraping
+
+I am using the O'Reilly book, "Web Scraping with Python" by Ryan Mitchell.
+It's a very readable book for a technical book.
+"""
+__author__ = 'rnzucker'
